2f212e83818c2c615a831e99817d52e56a737ee7
farfandavid <70915094+farfandavid@users.noreply.github.com>
2023-08-15 02:25:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <form>
+        <input placeholder="Type your PROMPT" type='text'></input>
+        <button>Generate</button>
+      </form>
+      <img src="https://img.freepik.com/vector-gratis/concepto-transformacion-digital-vector-fondo-microchip-tecnologia-ai_53876-112222.jpg?w=2000"></img>
+    </main>
+  );
+}
+
+export default App;
